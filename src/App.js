d7e901934f2b6c6aa6eeffd59bcc852b85e1989e
Kriyanshi Shah <kriyanshishah06@gmail.com>
2022-02-09 21:13:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <body>
+        <Navbar />
+      </body>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+function Navbar() {
+  return (
+    <div className="navbar-div">
+      <img className="logo-img" src="public\logo.svg" alt="" />
+    <div className="dropdown">
+      <img className="hamburger-icon" onclick="dropdown(this , 'show')" src="public/icon-hamburger.svg" alt="" />
+      <img className="close-icon" onclick="dropdown(this , 'close')" src="public/icon-close.svg" alt="" />
+      <div className="dropdown-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Blog</a>
+        <a href="#">Careers</a>
+      </div>
+    </div>
+    <button type="button" name="button"> Request Invite</button>
+    </div>
+  )
+}
+
+export default Navbar
