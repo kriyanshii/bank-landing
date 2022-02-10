2c0984a12eddd46cf6461c11ae67ae51e166463d
Kriyanshi Shah <kriyanshishah06@gmail.com>
2022-02-10 13:21:09 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@
 function Navbar() {
   return (
     <div className="navbar-div">
-      <img className="logo-img" src="" alt="" />
+      <img className="logo-img" src="https://saltpe.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F170e6aa8-7078-47d6-87f1-e2d4af3db3a8%2Fgrey2280x280.png?table=block&id=920b1e41-8fc3-444a-b758-ef07a07d9561&spaceId=f4ad0ece-f6b4-4c21-8dd7-8c113278c8d8&width=250&userId=&cache=v2" alt="" />
     <div className="dropdown">
       {/* <img className="hamburger-icon" onclick="dropdown(this , 'show')" src="public\icon-hamburger.svg" alt="" /> */}
       {/* <img className="close-icon" onclick="dropdown(this , 'close')" src="public\icon-close.svg" alt="" /> */}
